e1c0466d2ff3317ac815c6c3263e94ccb268b1bd
kalcryptev <abdallahkalenga1689@gmail.com>
2025-11-02 18:38:47 +0000

    chore: trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import Client from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
 const Page = () => {
-  return <div> Hello world. </div>;
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p> Loading... </p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default Page;
